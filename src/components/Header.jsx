c890ca66a5424fc6c421ca184b6d676cf33e6361
Your Name <darrehanrasool@gmail.com>
2025-05-17 11:29:35 +0530

    Dar Rehan Rasool
    sycomusiclover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { ReactDOM } from "react";
-function Header()
-{
-    return(
-        <header>
-             <h1>Google Keeper</h1>
-        </header>
- 
-    );
+function Header() {
+  return (
+    <header>
+      <h1>Google Keeper</h1>
+    </header>
+  );
 }
 export default Header;
